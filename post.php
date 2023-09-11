e7051e98a0e3be3787a589940cda6393f944f2ab
Sara Velez <svelez@playtechla.com>
2023-09-11 14:42:11 -0500

    se agrego campo

--- post.php
+++ post.php
@@ -6,8 +6,8 @@ if (!$jsonGasto) {
     exit("No hay datos");
 }
 $bd = include_once "bd.php";
-$sentencia = $bd->prepare("insert into centro_medico_hemo_hearth_diabetes (nombre, apellido, cedula, eps, sintomas, nivel) values (?,?,?,?,?,?)");
-$resultado = $sentencia->execute([$jsonGasto->nombre, $jsonGasto->apellido, $jsonGasto->cedula, $jsonGasto->eps, $jsonGasto->sintomas, $jsonGasto->nivel]);
+$sentencia = $bd->prepare("insert into centro_medico_hemo_hearth_diabetes (nombre, apellido, cedula, eps, sintomas, glicemia, nivel) values (?,?,?,?,?,?,?)");
+$resultado = $sentencia->execute([$jsonGasto->nombre, $jsonGasto->apellido, $jsonGasto->cedula, $jsonGasto->eps, $jsonGasto->sintomas, $jsonGasto->glicemia, $jsonGasto->nivel]);
 echo json_encode([
     "resultado" => $resultado,
 ]);
