b0e12b35503c3f3b9febb3e11db05b9f79d0069d
Sara Velez <svelez@playtechla.com>
2023-09-18 16:37:07 -0500

    cambio en campo

--- getAll.php
+++ getAll.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:4200");
 $bd = include_once "bd.php";
-$sentencia = $bd->query("select id, nombre, apellido, cedula, eps, sintomas,glicemia, nivel from centro_medico_hemo_hearth_diabetes");
+$sentencia = $bd->query("select id, nombre, apellido, cedula, eps, sintomas, nivel from centro_medico_hemo_hearth_diabetes");
 $centro = $sentencia->fetchAll(PDO::FETCH_OBJ);
 echo json_encode($centro);
 ?>
